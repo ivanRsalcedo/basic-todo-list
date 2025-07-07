4cff9d24c8606e1367de5800766dfab2814ee414
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-07 13:13:40 -0400

    Initial commit: Todo app with add/remove, DOM & Flexbox practice

---
+++ script.js
@@ -0,0 +1,31 @@
+const list = document.querySelector('#list');
+const input = document.querySelector('#input-field');
+const btnAdd = document.querySelector('#btn-add');
+
+btnAdd.addEventListener("click", () => {
+    if (input.value !== '') {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+        const taskName = document.createElement('p');
+        taskName.textContent = input.value;
+        const btnDone = document.createElement('button');
+        btnDone.classList.add('btn-done');
+        btnDone.textContent = '✓';
+        btnDone.addEventListener("click", () => {
+            list.removeChild(listItem);
+        });
+
+        listItem.appendChild(btnDone);
+        listItem.appendChild(taskName);
+
+        list.appendChild(listItem);
+        input.focus();
+    } else
+        input.placeholder = 'Enter task name here';
+});
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        btnAdd.click();
+    }
+});
